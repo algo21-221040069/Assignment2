1f4e0eaa78f53c32208c62f51e067ed6a41459fb
algo21-221040069 <100129194+algo21-221040069@users.noreply.github.com>
2022-03-29 12:30:53 +0800

    Add files via upload

---
+++ dataset/dataset_generation.py
@@ -0,0 +1,91 @@
+import sys
+from pathlib import Path
+
+import QUANTAXIS as qa
+
+_project_root = Path().cwd().parent
+sys.path.append(str(_project_root))
+
+from dataset_management.tools import construct_training_data, load_dataset, visualize_label_distribution, \
+    save_dataset_as_tfrecords, fetch_market_caps
+
+if __name__ == '__main__':
+    freq = '60min'
+    start_date = "2017-01-01"
+    end_date = "2020-12-31"
+
+    dataset_dir = Path().home() / 'datasets' / 'quant' / f'crypto{freq}_std'
+    split_ratio = .05
+
+    params = dict(
+        n=144,
+        t=12,
+        k_up=1.,
+        norm=True,
+        delta_type='volatility'
+    )
+
+    top_n = 20
+    market_caps = fetch_market_caps()
+    coins = [s['symbol'] for s in market_caps[:top_n]]
+    for coin in coins:
+        symbol = f'BINANCE.{coin}'
+        print(f"[INFO] Loading {symbol}@{freq} data: {start_date} ~ {end_date}...")
+        data = qa.QA_fetch_cryptocurrency_min_adv(symbol, start=start_date, end=end_date, frequence=freq).data.dropna()
+        dataset = construct_training_data(data, **params)
+        dataset_length = len(data) - (params['n'] + params['t']) + 1
+        print(f"[INFO] creating tfrec files", flush=True)
+        save_dataset_as_tfrecords(
+            dataset, dataset_dir,
+            split_ratio=split_ratio,
+            dataset_length=dataset_length,
+            buffer_size=1024)
+
+    trainset = load_dataset(dataset_dir / 'train')
+    validset = load_dataset(dataset_dir / 'valid')
+
+    visualize_label_distribution(trainset)
+    visualize_label_distribution(validset)
+    """
+300708it [01:51, 2700.50it/s]
+label -1: 50,253
+label 0: 195,366
+label 1: 55,089
+15836it [00:07, 2104.82it/s]
+label -1: 2,648
+label 0: 10,287
+label 1: 2,901
+    """
+
+    # start_date = '2012-01-01'
+    # end_date = '2018-12-31'
+    # params = dict(
+    #     n=120,
+    #     t=10,
+    #     k_up=1.,
+    #     norm=True
+    # )
+    # dataset_dir = Path().home() / 'datasets' / 'quant' / f'stock1d'
+    # split_ratio = .001
+    # stock_list = qa.QA_fetch_stock_list_adv()
+    # for i, (_, row) in tqdm(enumerate(stock_list.iterrows()), total=len(stock_list), disable=False):
+    #     symbol = row['code']
+    #     queryset = qa.QA_fetch_stock_day_adv(symbol, start=start_date, end=end_date)
+    #     if queryset is None or len(queryset) < (params['n'] + params['t']):
+    #         continue
+    #     # print(f"({i + 1}/{len(stock_list)}): {row['name']}({symbol:s})", end='')
+    #     candlesticks = queryset.to_qfq().data.dropna()
+    #     dataset_length = len(candlesticks) - (params['n'] + params['t']) + 1
+    #     dataset = construct_training_data(candlesticks, **params, shuffle=True)
+    #     save_dataset_as_tfrecords(
+    #         dataset, dataset_dir,
+    #         split_ratio=split_ratio,
+    #         dataset_length=dataset_length,
+    #         verbose=False, buffer_size=1024)
+
+'''
+A股分布：
+label -1: 608,183
+label 0: 2,655,274
+label 1: 678,669
+'''
--- dataset/tools.py
+++ dataset/tools.py
@@ -1,1 +1,211 @@
+import json
+from datetime import datetime
+from pathlib import Path
+from random import shuffle
 
+import numpy as np
+import pandas as pd
+import requests
+import tensorflow as tf
+from matplotlib import pyplot as plt
+from tqdm import tqdm
+
+from dataset_management.tfannotation import read_tfrecord, create_tfrecord
+from utilities.funcs import create_file_name, indicator_ATR
+from utilities.visualizations import draw_trading_data
+
+
+def normalize_ohlcv(data, history_size):
+    anchor_price = data['close'].iloc[0]
+    max_vol = data['volume'].iloc[:history_size].max()
+    min_vol = data['volume'].iloc[:history_size].min()
+    data[['open', 'high', 'low', 'close']] /= anchor_price
+    data['volume'] = (data['volume'] - min_vol) / (max_vol - min_vol)
+    if data.isnull().values.any():
+        raise ValueError("[ERROR] nan detected during data generation")
+    return data
+
+
+def segment_trading_data(data: pd.DataFrame, n: int = 144, t: int = 6, norm=False, shuffle=True):
+    """ Slide the given trading data and generate segments
+        data:   the trading data formatted as QUANTAXIS DataStruct
+        n:      history window size, default to 144 (12 hours of 5min data)
+        t:      future window size, default to 6 (half hour of 5min data)
+        norm:   specify whether to normalize the input data
+        :return : generator of (history, future) pair
+    """
+    window_size = n + t
+    indices = np.arange(0, len(data) - window_size + 1)
+    if shuffle:
+        np.random.shuffle(indices)
+
+    for i in indices:
+        segment = data.iloc[i: i + window_size].copy()
+        if norm:
+            segment = normalize_ohlcv(segment, n)
+
+        history = segment[:n]
+        future = segment[n:]
+        yield history, future
+
+
+def compute_label(history: pd.DataFrame, future: pd.DataFrame, k_up=1., k_lo=None, delta_type='volatility'):
+    if k_lo is None:
+        k_lo = k_up
+    last_bar = history.iloc[-1]
+    last_price = last_bar['close']
+    volatility = history['close'].std(ddof=0)
+    df = indicator_ATR(history, n=len(history) // 2)
+    ATR = df.iloc[-1].ATR
+    delta = volatility if delta_type == 'volatility' else ATR
+    upper = k_up * delta + last_price
+    lower = -k_lo * delta + last_price
+    above, = np.nonzero((future['close'] > upper).values)
+    below, = np.nonzero((future['close'] < lower).values)
+    first_above = above[0] if len(above) else len(future) + 1
+    first_below = below[0] if len(below) else len(future) + 1
+    if first_above < first_below:
+        return 1
+    elif first_below < first_above:
+        return -1
+    return 0
+
+
+def construct_training_data(data: pd.DataFrame, n: int = 144, t: int = 6, k_up=1., k_lo=None, delta_type='volatility',
+                            norm=False, columns=None, shuffle=True):
+    if columns is None:
+        columns = ['open', 'high', 'low', 'close', 'volume']
+    columns = list(columns)
+    segments = segment_trading_data(data=data, n=n, t=t, norm=norm, shuffle=shuffle)
+    for history, future in segments:
+        y = compute_label(history, future, k_up=k_up, k_lo=k_lo, delta_type=delta_type)
+        hist = history[columns].values
+        fut = future[columns].values
+        yield hist.astype('float32'), y, fut.astype('float32')
+
+
+def save_dataset_as_tfrecords(dataset, dataset_dir, split_ratio=.1, dataset_length=None, verbose=True, buffer_size=1):
+    """ Save the dataset generator into tfrecord files
+
+    :param dataset: generator
+    :param dataset_dir: root directory of the dataset to be saved
+    :param split_ratio: the ratio of validation size to the total size
+    :param dataset_length: optional, specified only to display the progress
+    :param verbose: whether to display progress bar
+    :param buffer_size: the number of samples to be saved into a single tfrecord file
+    """
+    train_dir = Path(str(dataset_dir)) / 'train'
+    valid_dir = Path(str(dataset_dir)) / 'valid'
+    train_dir.mkdir(parents=True, exist_ok=True)
+    valid_dir.mkdir(parents=True, exist_ok=True)
+
+    train_count = 0
+    valid_count = 0
+    train_writer = tf.io.TFRecordWriter(str(train_dir / f"{create_file_name()}.tfrecords"))
+    valid_writer = tf.io.TFRecordWriter(str(valid_dir / f"{create_file_name()}.tfrecords"))
+    for i, (history, y, future) in tqdm(enumerate(dataset), total=dataset_length, disable=not verbose):
+        if train_count == buffer_size:
+            train_writer.close()
+            file_name = f"{create_file_name()}.tfrecords"
+            file_path = train_dir / file_name
+            train_writer = tf.io.TFRecordWriter(str(file_path))
+            train_count = 0
+        if valid_count == buffer_size:
+            valid_writer.close()
+            file_name = f"{create_file_name()}.tfrecords"
+            file_path = valid_dir / file_name
+            valid_writer = tf.io.TFRecordWriter(str(file_path))
+            valid_count = 0
+        serialized = create_tfrecord(history=history, label=y, future=future)
+        if i % (1 / split_ratio) < 1:
+            valid_writer.write(serialized)
+            valid_count += 1
+        else:
+            train_writer.write(serialized)
+            train_count += 1
+
+    train_writer.close()
+    valid_writer.close()
+
+
+def load_dataset(directory):
+    directory = Path(str(directory))
+    data_files = [str(p) for p in directory.rglob('*.tfrecords')]
+    print(f"[INFO] {len(data_files)} files found.", flush=True)
+    shuffle(data_files)
+
+    raw = tf.data.TFRecordDataset(data_files)
+    dataset = raw.map(read_tfrecord).shuffle(buffer_size=100)
+    return dataset
+
+
+def visualize_label_distribution(dataset):
+    labels = [-1, 0, 1]
+    label_counts = {k: 0 for k in labels}
+
+    for history, label, future in tqdm(dataset):
+        label = label.numpy()
+        label_counts[label] += 1
+
+    for lb in labels:
+        print(f"label {lb}: {label_counts[lb]:,d}")
+    pd.DataFrame(label_counts, columns=label_counts.keys(), index=[0]).plot.bar()
+    plt.show()
+
+
+def display_batch(batch_data):
+    history, label, future = batch_data
+    history = history.numpy()[0]
+    label = label.numpy()[0]
+    future = future.numpy()[0]
+    display_training_data(history, label, future)
+
+
+def display_training_data(history, label, future, k_up=1., k_lo=None):
+    N, T = len(history), len(future)
+    history_df = pd.DataFrame(history, columns=['open', 'high', 'low', 'close', 'volume'])
+    future_df = pd.DataFrame(future, columns=['open', 'high', 'low', 'close', 'volume'])
+    df = pd.concat([history_df, future_df])
+    df.index = list(map(datetime.fromtimestamp, range(N + T)))
+    df.index.name = 'datetime'
+    fig, axes = draw_trading_data(df, title=f"Restored data visualization, label = {label}",
+                                  figsize=(12, 6))
+
+    # 画TBM线
+    if k_lo is None:
+        k_lo = k_up
+    volatility = history_df['close'].std(ddof=0)
+    top = history_df['close'].iloc[-1] + volatility * k_up
+    bot = history_df['close'].iloc[-1] - volatility * k_lo
+
+    axes[0].axvline(x=N - .5, alpha=.4, c='white', ls='--')
+    axes[0].axhline(y=top, c='g', ls='--', linewidth=1.5)
+    axes[0].axhline(y=bot, c='r', ls='--', linewidth=1.5)
+
+    plt.show()
+
+
+def fetch_market_caps(sort=True):
+    api_url = "https://www.binance.com/exchange-api/v2/public/asset-service/product/get-products"
+    resp = requests.get(api_url, proxies={'https': '127.0.0.1:1087'})
+    content = json.loads(resp.content)
+    data = content['data']
+    pairs = []
+    for d in data:
+        if d['q'] != 'USDT':
+            continue
+        if d['st'] != 'TRADING' or not d['cs'] or not d['c']:
+            continue
+        symbol = d['s']
+        circulating_supply = float(d['cs'])
+        price = float(d['c'])
+        pairs.append({
+            "symbol": symbol,
+            "market_cap": circulating_supply * price,
+            "circulating_supply": circulating_supply,
+            "price": price,
+            "vol24h": float(d['qv']),
+        })
+    if sort:
+        pairs.sort(key=lambda i: i['market_cap'], reverse=True)
+    return pairs
